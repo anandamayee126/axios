d27f2ad7c3cc68c5776adfcf33246c785f97c3a4
Anandamayee Ghosh <ghosh.anandamayee>
2023-08-29 15:30:34 +0530

    revived the users

--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 let parent_ul= document.getElementById('users');
 let myForm= document.getElementById('my-form');
-myForm.addEventListener('submit',addTodo);
+window.addEventListener('DOMContentLoaded',addTodo);
 
 
 function addTodo(e) {
-    const obj={
-        name: e.target.name.value,
-        email: e.target.email.value
-    }
+    
     e.preventDefault();
-   
-    axios.post('https://crudcrud.com/api/e858b168d2d24e40beab622f2158e58f/appointmentData',obj)
-    .then((res)=>{console.log(res)})
+    var li;
+    //const arr=[];
+    axios.get('https://crudcrud.com/api/e858b168d2d24e40beab622f2158e58f/appointmentData')
+    .then((res)=>
+    {
+        console.log(res);
+        for(var i=0;i<res.data.length;i++)
+        {
+          li=displayUsers(res.data[i]);
+          parent_ul.append(li);
+        }
+    })
     .catch(err => console.error(err));
+    //const li=displayUsers();
+   
+    e.target.name.value="";
+    e.target.email.value="";
 }
-function showOutput(res) {
-    document.getElementById('res').innerHTML = `
-    <div class="card card-body mb-4">
-      <h5>Status: ${res.status}</h5>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Headers
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-      </div>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Data
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.data, null, 2)}</pre>
-      </div>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Config
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.config, null, 2)}</pre>
-      </div>
-    </div>
-  `;
-  }
+function displayUsers(data)
+{
+    const li= document.createElement('li');
+    //li.textContent=index+" ";
+    const span1= document.createElement('span');
+    const span2= document.createElement('span');
+
+    span1.textContent=data.name+" -> ";
+    span2.textContent=data.email+"  ";
+
+    const edit= document.createElement('button');
+    const delete_btn= document.createElement('button');
+    edit.className='edit';
+    edit.textContent='edit';
+    delete_btn.className='delete';
+    delete_btn.textContent='delete';
+
+    li.appendChild(span1);
+    li.appendChild(span2);
+    li.appendChild(edit);
+    li.appendChild(delete_btn);
+
+    return li;
+}
